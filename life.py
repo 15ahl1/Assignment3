4d5429f7b0c8cfd4095ae630f53009a293ee8bcf
root <root@Kanyes-Laptop.localdomain>
2019-03-26 16:45:20 -0400

    Algo Works

--- life.py
+++ life.py
@@ -1,3 +1,8 @@
+export DISPLAY=:0.0
+
+import Tkinter as tk
+
+
 def readInput(path):
     output = []
     with open(path, "r") as file:
@@ -11,16 +16,32 @@ def readInput(path):
     file.close()
     return output, generation
 
+#Writes board and generation to file
+def addToFile(path, board, gen):
+    with open(path, "a") as file:
+        file.write("Generation " + str(gen) + "\n")
+        for row in board:
+            temp = ""
+            for elem in row:
+                temp += (str(elem) + "")
+            file.write(temp + "\n")
+    file.close()
+
+#Formats and prints the board
 def printBoard(board):
     for row in board:
         for elem in row:
             print(elem),
         print("\n")
+    print("\n")
 
+
+#Calculates the number of live neighbours per square
 def calculateLiveNeighbours(board, x, y):
     count = 0
-    xLen = len(board[0])
-    yLen = len(board)
+    xLen = len(board)
+    yLen = len(board[0])
+
     #Up
     if y - 1 >= 0 and board[x][y - 1] == 1:
         count += 1
@@ -45,20 +66,12 @@ def calculateLiveNeighbours(board, x, y):
     #BottomRight
     if x + 1 < xLen and y + 1 < yLen and board[x + 1][y + 1] == 1:
         count += 1
-
-    print("Calculated Neighbours: " + str(count))
+    #print("Position (" + str(x) + ", " + str(y) + ") = " + str(count))
     return count
 
-def addToFile(path, board, gen):
-    with open(path, "a") as file:
-        file.write(str(gen) + "\n")
-        for row in board:
-            temp = ""
-            for elem in row:
-                temp += (str(elem) + "")
-            file.write(temp + "\n")
-    file.close()
 
+# Returns a table containing the number
+# of live neighbours for each square
 def onOffTable(board):
     xLen = len(board[0])
     yLen = len(board)
@@ -66,46 +79,44 @@ def onOffTable(board):
     for row in range(yLen):
         temp = []
         for elem in range(xLen):
-            temp.append(calculateLiveNeighbours(board, elem, row))
+            temp.append(calculateLiveNeighbours(board, row, elem))
         output.append(temp)
     return output
 
-
+#Returns the next generation of the current board
 def getNextGeneration(board):
     liveTable = onOffTable(board)
-    printBoard(liveTable)
-    print("\n")
     output = []
-    for i in range(len(board[0])):
+    for i in range(len(board)):
         temp = []
-        for j in range(len(board)):
-            if(liveTable[i][j] >= 2 and board[i][j] == 1):
+        for j in range(len(board[0])):
+            if(board[i][j] == 1 and liveTable[i][j] in range(2, 4)):
                 temp.append(1)
-            elif(liveTable[i][j] >= 3):
+            elif(liveTable[i][j] == 3):
                 temp.append(1)
             else:
                 temp.append(0)
         output.append(temp)
     return output
 
-
-
-
+#Driver
 def main():
+    #GUI
+    root = tk.Tk()
+    root.mainloop()
+
+
     board, generations = readInput("./inLife.txt")
-    print("Generations to Simulate: " + generations)
+    generations = int(generations)
+    print("Generations to Simulate: " + str(generations))
     print("Generation 0")
     printBoard(board)
     addToFile("./outLife.txt", board, 0)
-    next = getNextGeneration(board)
-    printBoard(next)
-    '''
+
     for gen in range(generations):
-        print("Generation " + (gen + 1))
+        print("Generation " + str(gen + 1))
         board = getNextGeneration(board)
         printBoard(board)
         addToFile("./outLife.txt", board, gen + 1)
-    '''
-
 
 main()
